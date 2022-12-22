61f538e2323087eab8acee35918f3d88b81ff6b4
dharlabwustl <atulkumar@wustl.edu>
2022-12-22 16:40:35 -0600

    Commit 2

--- downloadcodefromgithub.sh
+++ downloadcodefromgithub.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 cd /software1/
-git_link=${4}
+rm -r /software1/*
+git_link=${5}
 git clone ${git_link} #https://github.com/dharlabwustl/EDEMA_MARKERS_PROD.git
 y=${git_link%.git}
 git_dir=$(basename $y)
 mv ${git_dir}/* /software1/
 chmod +x /software1/*.sh
-
+#$SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST ${git_link} ${program_type}
 
 SESSION_ID=${1}
 XNAT_USER=${2}
 XNAT_PASS=${3}
-TYPE_OF_PROGRAM=${5}
+XNAT_HOST=${4}
+TYPE_OF_PROGRAM=${6}
 
-/software1/script_to_call_main_program.sh $SESSION_ID $XNAT_USER $XNAT_PASS ${TYPE_OF_PROGRAM}
+/software1/script_to_call_main_program.sh $SESSION_ID $XNAT_USER $XNAT_PASS ${XNAT_HOST} ${TYPE_OF_PROGRAM}
--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -3,5 +3,11 @@
 SESSION_ID=${1}
 XNAT_USER=${2}
 XNAT_PASS=${3}
-TYPE_OF_PROGRAM=${4}
+XNAT_HOST=${4}
+TYPE_OF_PROGRAM=${5}
+echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
+if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
+then
 /software1/call_Classifier_sessionlevel.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS https://snipr.wustl.edu
+fi
+
