c622046645014aa08eff2019205fad43a8432d29
dharlabwustl <atulkumar@wustl.edu>
2022-12-22 16:44:37 -0600

    Commit 2

--- builddockerimage.sh
+++ builddockerimage.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
- ./bashtowriteDockerfile.sh
+parent_dir=${1}
+ ${parent_dir}/bashtowriteDockerfile.sh
 command=""
-for x in *.json ;
+for x in ${parent_dir}/*.json ;
 do 
 	command="${command}   ${x}  "
 done
 echo $command
-python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/FROM_DOCUMENTS/docker-images/command2label.py  $command  >> ./Dockerfile
- imagename=$1
+python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/FROM_DOCUMENTS/docker-images/command2label.py  $command  >> ${parent_dir}/Dockerfile
+ imagename=$2
 #imagename=hosseinclassifier1
 docker login registry.nrg.wustl.edu
 #docker build -t docker/nrg-repo/sharmaatul11/${imagename} .
 #docker login registry.nrg.wustl.edu
 #docker push docker/nrg-repo/sharmaatul11/${imagename}
 # docker run -v $PWD:/mounted_directory -it   sharmaatul11/${imagename}  /bin/bash
-docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} .
+docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} ${parent_dir}
 docker push registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename}
