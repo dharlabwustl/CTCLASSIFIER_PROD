0cb422c704ee67da7cbb315e7632fdc454abd5c3
dharlabwustl <atulkumar@wustl.edu>
2023-07-10 12:07:31 -0500

    Commit 2

--- Classifier_session_level_v1_5July2023.py
+++ Classifier_session_level_v1_5July2023.py
@@ -3,13 +3,14 @@
 import os, sys, errno, shutil, uuid,json
 import math
 import glob
-import re
+import re,argparse
+import inspect
 import pandas as pd
 import requests
 import pydicom as dicom
 import subprocess
 from xnatSession import XnatSession
-import DecompressDCM
+import DecompressDCM,time
 import label_probability
 # Prep XNAT session
 XNAT_HOST = 'https://snipr.wustl.edu' #os.environ['XNAT_HOST']#
@@ -17,6 +18,13 @@ XNAT_USER = os.environ['XNAT_USER']
 XNAT_PASS = os.environ['XNAT_PASS']
 catalogXmlRegex = re.compile(r'.*\.xml$')
 
+def wait_for_file_tobe_written(file_path,time_to_wait):
+    # time_to_wait = 10
+    time_counter = 0
+    while not os.path.exists(file_path):
+        time.sleep(1)
+        time_counter += 1
+        if time_counter > time_to_wait:break
 def get_slice_idx(nDicomFiles):
     return min(nDicomFiles-1, math.ceil(nDicomFiles*0.7)) # slice 70% through the brain
 def get_metadata_session(sessionId):
@@ -27,6 +35,11 @@ def get_metadata_session(sessionId):
     xnatSession.close_httpsession()
     metadata_session=response.json()['ResultSet']['Result']
     return metadata_session
+def call_get_metadata_session_saveascsv(args):
+    sessionId=args.stuff[1]
+    filename=args.stuff[2]
+    get_metadata_session_saveascsv(sessionId,filename)
+
 def get_metadata_session_saveascsv(sessionId,filename):
     url = ("/data/experiments/%s/scans/?format=json" %    (sessionId))
     xnatSession = XnatSession(username=XNAT_USER, password=XNAT_PASS, host=XNAT_HOST)
@@ -180,6 +193,85 @@ def run_classifier(sessionDir, rawDir, jpgDir, sessionId, scanId, xnatSession):
             errStr = "PERMISSION DENIED"
         raise Exception("%s attempting to set series_class for %s %s to '%s': %s" % 
             (errStr, sessionId, scanId, label, response.text))
+def call_classifier_v1(args):
+    return_value=0
+    try:
+        sessionDir = args.stuff[1]
+        workingDir = args.stuff[2]
+        sessionId = args.stuff[3]
+        command = "echo  success at : " +  inspect.stack()[0][3]  + " >> " + "/workingoutput/error.txt"
+        subprocess.call(command,shell=True)
+        print("I SUCCEEDED AT ::{}".format(inspect.stack()[0][3]))
+        classifier_v1(sessionDir,workingDir,sessionId)
+    except Exception:
+        command = "echo  FAILURE at : " +  inspect.stack()[0][3]  + " >> " + "/workingoutput/error.txt"
+        subprocess.call(command,shell=True)
+        command = "echo  ERROR : " +  Exception  + " >> " + "/workingoutput/error.txt"
+        subprocess.call(command,shell=True)
+
+
+    return 1
+def classifier_v1(sessionDir,workingDir,sessionId):
+    # sessionDir = sys.argv[1]
+    # workingDir = sys.argv[2]
+    # sessionId = sys.argv[3]
+    ##############################################
+    try:
+        metadata_session=get_metadata_session(sessionId)
+        for x in metadata_session:
+            # if int(x['ID']) == scanId:
+            scanId=x['ID']
+
+
+            try:
+
+                ###########################################################
+                # xnatSesDir = sys.argv[4]
+                # scans = [sys.argv[4]] #sys.argv[5].split()
+                # scanId=scans[0]
+                # Make working dirs
+                rawDir = os.path.join(workingDir, 'RAW')
+                os.makedirs(rawDir, exist_ok = True)
+                jpgDir = os.path.join(workingDir, 'JPG' )
+                os.makedirs(jpgDir, exist_ok = True)
+                command="rm -r  " + rawDir + "/*"
+                subprocess.call(command,shell=True)
+                command="rm -r  " + jpgDir + "/*"
+                subprocess.call(command,shell=True)
+                command="rm -r /NIFTIFILEDIR/*"
+                subprocess.call(command,shell=True)
+                # for x in range(1,5):
+                #     print(sys.argv[x])
+                command="rm -r /ZIPFILEDIR/*"
+                subprocess.call(command,shell=True)
+                command="rm -r /DICOMFILEDIR/*"
+                subprocess.call(command,shell=True)
+                # for x in range(10):
+                #     print("{}:XNAT_HOST".format(XNAT_HOST))
+                xnatSession = XnatSession(username=XNAT_USER, password=XNAT_PASS, host=XNAT_HOST)
+                run_classifier(sessionDir, rawDir, jpgDir, sessionId, scanId, xnatSession)
+                # Handle DICOM files that are not stored in a directory matching their XNAT scanId
+                xnatSession.close_httpsession()
+            except Exception as e: # work on python 3.x
+                print('Exception occured: '+ str(e))
+                continue
+        command = "echo  SUCCESS at : " +  inspect.stack()[0][3]  + " >> " + "/workingoutput/error.txt"
+        subprocess.call(command,shell=True)
+    except Exception:
+        command = "echo  FAILURE at : " +  inspect.stack()[0][3]  + " >> " + "/workingoutput/error.txt"
+        subprocess.call(command,shell=True)
+        command = "echo  ERROR : " +  Exception  + " >> " + "/workingoutput/error.txt"
+        subprocess.call(command,shell=True)
+def main():
+    print("WO ZAI ::{}".format("main"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('stuff', nargs='+')
+    args = parser.parse_args()
+    name_of_the_function=args.stuff[0]
+    return_value=0
+    if name_of_the_function == "call_classifier_v1":
+        return_value=call_classifier_v1(args)
+    return  return_value
 
 
 if __name__ == '__main__':
--- call_Classifier_sessionlevel.sh
+++ call_Classifier_sessionlevel.sh
@@ -5,9 +5,20 @@ export XNAT_USER=${2}
 export XNAT_PASS=${3}
 export XNAT_HOST=${4} #"https://snipr-dev-test1.nrg.wustl.edu"
 sessionId=$1 #SNIPR_E03539 #SNIPR_E03526
+
+#file_to_wait=$1
+#while [ ! –e ${file_to_wait} ]
+#do
+#  sleep 1
+#done
+#
+#echo “${file_to_wait} has been created”
 # scanID=$2 #8
 # rm -r /ZIPFILEDIR/*
 # cp /mounted_directory/Classifier_wholeSession.py /run/Classifier_wholeSession.py 
 # cd /run/
 # echo $PWD
-python /software1/Classifier_session_level.py /input /working ${sessionId}  #${scanID} 
+#python /software1/Classifier_session_level.py /input /working ${sessionId}  #${scanID}
+
+call_classifier_v1_arguments=('call_classifier_v1' /input /working ${sessionId})
+outputfiles_present=$(python /software1/Classifier_session_level_v1_5July2023.py  "${call_classifier_v1_arguments[@]}")
