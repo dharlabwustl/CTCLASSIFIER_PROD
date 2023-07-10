088c8cd2cfb139d1c736d32898e5185efd4e8d5c
dharlabwustl <atulkumar@wustl.edu>
2023-07-10 12:34:17 -0500

    Commit 2

--- call_Classifier_sessionlevel.sh
+++ call_Classifier_sessionlevel.sh
@@ -5,7 +5,24 @@ export XNAT_USER=${2}
 export XNAT_PASS=${3}
 export XNAT_HOST=${4} #"https://snipr-dev-test1.nrg.wustl.edu"
 sessionId=$1 #SNIPR_E03539 #SNIPR_E03526
+function wait_for_file()
 
+{
+ local file_to_wait=${1} #${filename}
+ local sleep_second=1
+ local  sleep_second_counter=0
+ while [ ! -e ${file_to_wait} ]
+ do
+   sleep ${sleep_second}
+   sleep_second_counter=$((sleep_second_counter +1 ))
+   if [ ${sleep_second_counter} -gt 10 ] ; then
+     echo sleep_second_counter::${sleep_second_counter}
+     break
+   fi
+   echo sleep_second_counter::${sleep_second_counter}
+ done
+
+}
 #file_to_wait=$1
 #while [ ! –e ${file_to_wait} ]
 #do
@@ -23,16 +40,4 @@ output_directory=/output/
 filename=${output_directory}/${sessionId}.csv
 call_get_metadata_session_saveascsv_arguments=('call_get_metadata_session_saveascsv' ${sessionId} ${filename})
 outputfiles_present=$(python /software1/Classifier_session_level_v1_5July2023.py  "${call_get_metadata_session_saveascsv_arguments[@]}")
-file_to_wait=${filename}
-sleep_second=1
-sleep_second_counter=0
-while [ ! -e ${file_to_wait} ]
-do
-  sleep ${sleep_second}
-  sleep_second_counter=$((sleep_second_counter +1 ))
-  if [ ${sleep_second_counter} -gt 10 ] ; then
-    echo sleep_second_counter::${sleep_second_counter}
-    break
-  fi
-  echo sleep_second_counter::${sleep_second_counter}
-done
+wait_for_file ${filename}
