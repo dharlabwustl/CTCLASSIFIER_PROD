ebf091a460a4e88856c8a492d1fedf91a5a0ab99
dharlabwustl <atulkumar@wustl.edu>
2023-07-10 13:08:21 -0500

    Commit 2

--- call_Classifier_sessionlevel.sh
+++ call_Classifier_sessionlevel.sh
@@ -56,5 +56,5 @@ while IFS=',' read -ra array; do
 #    dir_to_save=args.stuff[3]
     call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${URI} ${resource_dir} ${dir_to_receive_the_data} ${output_csvfile})
 #    outputfiles_present=$(python /software1/Classifier_session_level_v1_5July2023.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-  done < <(tail -n +2 "${filename}")
+  done < <(tail -n +2 "${dir_to_receive_the_data}/${output_csvfile}")
 done < <(tail -n +2 "${filename}")
