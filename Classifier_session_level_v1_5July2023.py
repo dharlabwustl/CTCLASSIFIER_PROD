f89c43534e38adb0e5e00981bf33dbfc02ff94fa
dharlabwustl <atulkumar@wustl.edu>
2023-07-10 16:27:41 -0500

    Commit 2

--- Classifier_session_level_v1_5July2023.py
+++ Classifier_session_level_v1_5July2023.py
@@ -169,7 +169,7 @@ def get_dicom_using_xnat_10_July_2023(sessionId, scanId,xnatSession,sessionDir='
             # return selDicom,
         df_scan=sort_dicom_list_DF(df_scan)
         # #     # Get 70% file and ensure it exists
-        selDicomAbs =df_scan.at[get_slice_idx(nDicomFiles),"URI"]   # result[get_slice_idx(nDicomFiles)]['absolutePath']
+        selDicomAbs =df_scan.iloc[int(get_slice_idx(nDicomFiles)),"URI"]   # result[get_slice_idx(nDicomFiles)]['absolutePath']
         # print(selDicomAbs)
         # download_a_singlefile_with_URIString(selDicomAbs,os.path.basename(selDicomAbs),sessionDir)
         # selDicom=os.path.join(sessionDir,os.path.basename(selDicomAbs))
