258553293ebd5abe74355abb1cc92c606ba74a09
dharlabwustl <atulkumar@wustl.edu>
2023-08-24 14:51:23 -0500

    Commit 2

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -6,8 +6,12 @@ XNAT_PASS=${3}
 export XNAT_HOST=${4}
 TYPE_OF_PROGRAM=${5}
 echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
-#if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
-#then
+if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
+then
 /software1/call_Classifier_sessionlevel.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS ${4}
-#fi
+fi
+if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
+then
+/software1/call_Classifier_sessionlevel_SAH.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS ${4}
+fi
 
