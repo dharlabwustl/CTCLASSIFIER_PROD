f194794889bbda683bc58b49cbc6955d8d0def0a
dharlabwustl <atulkumar@wustl.edu>
2023-08-24 17:15:41 -0500

    Commit 2

--- Classifier_session_level_SAH.py
+++ Classifier_session_level_SAH.py
@@ -130,23 +130,24 @@ def run_classifier(sessionDir, rawDir, jpgDir, sessionId, scanId, xnatSession):
     # Change value of series_class in XNAT
     # url = ("/data/experiments/%s/scans/%s?xsiType=xnat:mrScanData&xnat:imageScanData/series_class=%s" % 
     #     (sessionId, scanId, label))
-    try:
-        url = ("/data/experiments/%s/scans/%s?xsiType=xnat:ctScanData&type=%s" % (sessionId, scanId, label))
-        # xnatSession.renew_httpsession()
+    # try:
+    url = ("/data/experiments/%s/scans/%s?xsiType=xnat:ctScanData&type=%s" % (sessionId, scanId, label))
+    # xnatSession.renew_httpsession()
+    response = xnatSession.httpsess.put(xnatSession.host + url)
+    command="echo successful at :: {}::maskfilename::{} >> /software1/error.txt".format(inspect.stack()[0][3],"run_classifier")
+    subprocess.call(command,shell=True)
+    if response.status_code != 200 or response.status_code != 201:
+    # except:
+    #     try:
+        url = ("/data/experiments/%s/scans/%s?xsiType=xnat:mrScanData&type=%s" % (sessionId, scanId, label))
+    # xnatSession.renew_httpsession()
         response = xnatSession.httpsess.put(xnatSession.host + url)
         command="echo successful at :: {}::maskfilename::{} >> /software1/error.txt".format(inspect.stack()[0][3],"run_classifier")
         subprocess.call(command,shell=True)
-    except:
-        try:
-            url = ("/data/experiments/%s/scans/%s?xsiType=xnat:mrScanData&type=%s" % (sessionId, scanId, label))
-    # xnatSession.renew_httpsession()
-            response = xnatSession.httpsess.put(xnatSession.host + url)
-            command="echo successful at :: {}::maskfilename::{} >> /software1/error.txt".format(inspect.stack()[0][3],"run_classifier")
-            subprocess.call(command,shell=True)
-        except:
-            command="echo failed at :: {}::maskfilename::{} >> /software1/error.txt".format(inspect.stack()[0][3],"run_classifier")
-            subprocess.call(command,shell=True)
-            pass
+        # except:
+        #     command="echo failed at :: {}::maskfilename::{} >> /software1/error.txt".format(inspect.stack()[0][3],"run_classifier")
+        #     subprocess.call(command,shell=True)
+            # pass
     if response.status_code == 200 or response.status_code == 201:
         print("Successfully set type for %s scan %s to '%s'" % (sessionId, scanId, label))
     else:
