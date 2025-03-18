d9416c69b0f1628fd96860f17755adf467f55b57
dharlabwustl <atulkumar@wustl.edu>
2025-03-18 15:56:09 -0500

    Commit 2

--- Classifier_session_levelFeb062025.py
+++ Classifier_session_levelFeb062025.py
@@ -165,47 +165,34 @@ def run_classifier(sessionDir, rawDir, jpgDir, sessionId, scanId, xnatSession):
     DecompressDCM.decompress(selDicom, selDicomDecompr)
     # Classify it
     label = label_probability.classify(selDicomDecompr, jpgDir, scanId, nDicomFiles)
-    # print("Scan classification for %s scan %s is '%s'" % (sessionId, scanId, label))
-    # # Change value of series_class in XNAT
-    # # url = ("/data/experiments/%s/scans/%s?xsiType=xnat:mrScanData&xnat:imageScanData/series_class=%s" %
-    # #     (sessionId, scanId, label))
-    # url = ("/data/experiments/%s/scans/%s?xsiType=xnat:ctScanData&type=%s" % (sessionId, scanId, label))
-    # # #xnatSession.renew_httpsession()
-    # # print(xnatSession.httpsess.get(xnatSession.host + url))
-    # command = f' curl  -u   {XNAT_USER}:{XNAT_PASS}  -X PUT    {XNAT_HOST}{url}'
-    # subprocess.call(command,shell=True)
-    # response=200
-    # # try:
-    # #     response = xnatSession.httpsess.put(xnatSession.host + url)
-    # # except:
-    # #     try:
-    # #         command = f' curl  -u   {XNAT_USER}:{XNAT_PASS}  -X PUT    {XNAT_HOST}{url}'
-    # #         subprocess.call(command,shell=True)
-    # #     except:
-    # #         pass
-    #
-    # if response.status_code == 200 or response.status_code == 201:
-    #     print("Successfully set type for %s scan %s to '%s'" % (sessionId, scanId, label))
-    #     # session_id=session_name=sessionId
-    #     # scan_id=scan_name=scanId
-    #     # try:
-    #     #     insert_data(session_id, session_name, scan_id, scan_name)
-    #     #
-    #     #     # Update or create column
-    #     #     column_name ="TESTING_INSERTION" #  "volume"  # Specify the new column name
-    #     #     column_value ="YES" #  "200"  # Value to be set in the new column
-    #     #     update_or_create_column_with_given_session_only(session_id, column_name, column_value)
-    #     #
-    #     #     # update_or_create_column(session_id, scan_id, column_name, column_value,session_name,scan_name)
-    #     # except:
-    #     #     pass
-    # else:
-    #
-    #     errStr = "ERROR"
-    #     if response.status_code == 403 or response.status_code == 404:
-    #         errStr = "PERMISSION DENIED"
-    #     raise Exception("%s attempting to set type for %s %s to '%s': %s" %
-    #                     (errStr, sessionId, scanId, label, response.text))
+    print("Scan classification for %s scan %s is '%s'" % (sessionId, scanId, label))
+    # Change value of series_class in XNAT
+    # url = ("/data/experiments/%s/scans/%s?xsiType=xnat:mrScanData&xnat:imageScanData/series_class=%s" %
+    #     (sessionId, scanId, label))
+    url = ("/data/experiments/%s/scans/%s?xsiType=xnat:ctScanData&type=%s" % (sessionId, scanId, label))
+    # #xnatSession.renew_httpsession()
+    response = xnatSession.httpsess.put(xnatSession.host + url)
+    if response.status_code == 200 or response.status_code == 201:
+        print("Successfully set type for %s scan %s to '%s'" % (sessionId, scanId, label))
+        # session_id=session_name=sessionId
+        # scan_id=scan_name=scanId
+        # try:
+        #     insert_data(session_id, session_name, scan_id, scan_name)
+        #
+        #     # Update or create column
+        #     column_name ="TESTING_INSERTION" #  "volume"  # Specify the new column name
+        #     column_value ="YES" #  "200"  # Value to be set in the new column
+        #     update_or_create_column_with_given_session_only(session_id, column_name, column_value)
+        #
+        #     # update_or_create_column(session_id, scan_id, column_name, column_value,session_name,scan_name)
+        # except:
+        #     pass
+    else:
+        errStr = "ERROR"
+        if response.status_code == 403 or response.status_code == 404:
+            errStr = "PERMISSION DENIED"
+        raise Exception("%s attempting to set type for %s %s to '%s': %s" %
+                        (errStr, sessionId, scanId, label, response.text))
 
 
 if __name__ == '__main__':
